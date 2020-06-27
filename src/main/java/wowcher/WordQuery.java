fa42f75941bfcc096ccfb17044155834ecf0cff3
802213 <duminda.gamlath@hotelbeds.com>
2020-06-27 12:11:13 +0100

    Added Lambda function

--- src/main/java/wowcher/WordQuery.java
+++ src/main/java/wowcher/WordQuery.java
@@ -4,10 +4,12 @@ import java.time.LocalTime;
 import java.time.temporal.TemporalAccessor;
 import java.time.temporal.TemporalQuery;
 import java.util.StringJoiner;
+import java.util.function.BiPredicate;
 
 import wowcher.constants.TimeConstants;
 
 public class WordQuery implements TemporalQuery<String> {
+
     @Override
     public String queryFrom(TemporalAccessor temporal) {
         LocalTime t = LocalTime.from(temporal);
@@ -18,24 +20,28 @@ public class WordQuery implements TemporalQuery<String> {
         } else if (t.compareTo(LocalTime.NOON) == 0) {
             timeString.add(TimeConstants.MIDDAY);
         } else {
-            appendWord(t.getHour(), 20, timeString);
+            timeString.add(wordFinder(t.getHour(), 20, (a, b) -> (a <= b || b == 0)));
             int minute = t.getMinute();
-            if (minute > 20) {
-                appendWord(minute, Math.floorDiv(minute, 10) * 10, timeString);
-            } else if (minute > 0) {
-                timeString.add(TimeConstants.timeMap.get(minute));
+            if (minute > 0) {
+                timeString.add(wordFinder(minute, minute > 20 ? Math.floorDiv(minute, 10) * 10 : 0, (a, b) -> (a <= b || b == 0)));
             }
         }
 
         return timeString.toString();
     }
 
-    private void appendWord(int number, int div, StringJoiner wordStr) {
-        if (number > div) {
-            wordStr.add(TimeConstants.timeMap.get(div));
-            wordStr.add(TimeConstants.timeMap.get(Math.floorMod(number, div)));
+    private static String wordFinder(
+            int a, int b, BiPredicate<Integer, Integer> tester) {
+        StringBuilder word = new StringBuilder();
+        if (tester.test(a, b)) {
+            word.append(TimeConstants.timeMap.get(a));
         } else {
-            wordStr.add(TimeConstants.timeMap.get(number));
+            word.append(TimeConstants.timeMap.get(b));
+            word.append(" ");
+            word.append(TimeConstants.timeMap.get(Math.floorMod(a, b)));
         }
+        return word.toString();
     }
 }
+
+
